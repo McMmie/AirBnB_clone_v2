acde4d6cfdf69e930dd2372948cfe2084dffcad4
McMmie <tharausamuel01@gmail.com>
2023-12-07 10:27:47 +0000

    automation script for my static web pages

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,13 +3,13 @@
 
 STATIC="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n}"
 MATCH="server_name _;"
-mkdir /data/
-mkdir /data/web_static/
-mkdir /data/web_static/releases/
-mkdir /data/web_static/releases/test/
-mkdir /data/web_static/shared/
 
-echo "<html>
+sudo apt -y update
+sudo apt -y install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+sudo echo "<html>
   	<head>
     	</head>
 	<body>
@@ -17,7 +17,7 @@ echo "<html>
 	</body>
        </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -fs /data/web_static/releases/test /data/web_static/current
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i.backup "s|$MATCH|$STATIC|" /etc/nginx/sites-available/default
 
