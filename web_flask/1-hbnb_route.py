ac58d979caaf9cd961a3ad71e398246696d30bfb
McMmie <tharausamuel01@gmail.com>
2023-12-28 00:42:36 +0000

    innitial commit

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""starts a flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """the function that displays on message on the browser
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """the function that displays on message on the browser
+    """
+    return "HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
