851540363c2da01ba8b0d1150f3422ff8331987c
McMmie <tharausamuel01@gmail.com>
2024-01-02 00:31:05 +0000

    database added

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,8 @@
 -- Connect to MySQL as root
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
